8318c540ef8c668ff2157361783a6fcbecbf6c02
Rafael Gervasio <rafaelgaspargervasio@gmail.com>
2023-09-22 13:49:55 +0100

    Exercise 3 completed

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,4 +1,16 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    newString = ''
+    
+    for (i=(string.length-1); i>=0; i--){
+        
+        newString += string[i]
+        
+        // console.log(newString)
+        // console.log(i)
+
+    }
+
+    return newString
 
 };
 
