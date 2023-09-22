94ce5d6ca2d32cc30deadd750e8cd066e93f403e
Rafael Gervasio <rafaelgaspargervasio@gmail.com>
2023-09-22 17:41:25 +0100

    Exercise 5 completed

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,25 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    sum = 0
+    
+    if (b<a){
+        c = b
+        b = a
+        a = c
+    }
 
+    if(a<0 || b<0){
+        return "ERROR"
+    }
+
+    if (typeof a != 'number' || typeof b != 'number'){
+        return "ERROR"
+    }
+
+    for (i=a; i<=b; i++){
+        sum+=i
+    }
+    
+    return sum
 };
 
 // Do not edit below this line
