e3259ed5dc6fbfad0d643d7c37eea839999dfd9b
Rafael Gervasio <rafaelgaspargervasio@gmail.com>
2023-09-22 17:28:22 +0100

    Exercise 4 completed

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -2,12 +2,7 @@ const reverseString = function(string) {
     newString = ''
     
     for (i=(string.length-1); i>=0; i--){
-        
         newString += string[i]
-        
-        // console.log(newString)
-        // console.log(i)
-
     }
 
     return newString
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,15 @@
-const removeFromArray = function() {
-
-};
+function removeFromArray (array, ...args){
+        return array.filter(x => !args.includes(x))
+    }
 
 // Do not edit below this line
 module.exports = removeFromArray;
+
+
+// ✓ removes a single value (4 ms)
+// ✕ removes multiple values (6 ms)
+// ✓ ignores non present values (1 ms)
+// ✕ ignores non present values, but still works (1 ms)
+// ✕ can remove all values (3 ms)
+// ✕ works with strings (1 ms)
+// ✕ only removes same type (2 ms)
