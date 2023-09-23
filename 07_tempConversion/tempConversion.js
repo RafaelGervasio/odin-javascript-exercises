4ba827d706fd924d6f9b801335f6f3065c16f81d
Rafael Gervasio <rafaelgaspargervasio@gmail.com>
2023-09-23 10:18:40 +0100

    Completed exercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,44 @@
-const convertToCelsius = function() {
-};
+const convertToCelsius = function(fahrenheitTemp) {
+  temp = fahrenheitTemp - 32
+  celsiusTemp = temp * 5/9
 
-const convertToFahrenheit = function() {
+  let isWhole 
+  if (celsiusTemp % 0 == 0){
+    isWhole = true
+  }
+  else{
+    isWhole = false
+  }
+
+  if (isWhole==true){
+    return celsiusTemp 
+  }
+  else{
+    return Number(celsiusTemp.toFixed(1))
+  }
+
+
+}
+
+const convertToFahrenheit = function(celsiusTemp) {
+  temp = celsiusTemp*1.8
+  fahrenheitTemp = temp + 32
+
+  let isWhole 
+  if (fahrenheitTemp % 0 == 0){
+    isWhole = true
+  }
+  else{
+    isWhole = false
+  }
+
+
+  if (isWhole==true){
+    return fahrenheitTemp 
+  }
+  else{
+    return Number(fahrenheitTemp.toFixed(1))
+  }
 };
 
 // Do not edit below this line
